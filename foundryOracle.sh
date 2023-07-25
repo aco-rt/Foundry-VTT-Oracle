4d774b31aea73e216d35582ba182aa4da64e9bae
aco-rt <48486853+aco-rt@users.noreply.github.com>
2023-07-25 09:34:28 +0530

    Update foundryOracle.sh
    updated nodejs to the latest LTS of v 18. Some advanced users may want to upgrade this to 20 in the future if needed.

--- foundryOracle.sh
+++ foundryOracle.sh
@@ -9,7 +9,7 @@ sudo iptables -I INPUT 6 -m state --state NEW -p tcp --match multiport --dports 
 sudo netfilter-persistent save
 # install nodejs, pm2, nano, unzip
 sudo apt install curl nano unzip -y
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install pm2 -g
 #allow pm2 to start and stop after reboot
