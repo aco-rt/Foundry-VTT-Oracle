2044a97dde796b5a862f073b62f89792dce9df84
aco-rt <grpnewaa@gmail.com>
2025-05-03 11:19:00 -0600

    v12 to v13
    updates to files to add support for 12 and 13.

---
+++ foundryOraclev12.sh
@@ -0,0 +1,52 @@
+#!/bin/bash    
+# chmod a+x /where/i/saved/it/foundryInstall.sh
+# Foundry Installation on Oracle
+# update the system and remove older packages
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean
+#update the iptables to open ports 80, 443, 30000
+sudo iptables -I INPUT 6 -m state --state NEW -p tcp --match multiport --dports 80,443,30000 -j ACCEPT
+# save this configuration
+sudo netfilter-persistent save
+# install nodejs, pm2, nano, unzip
+sudo apt install curl nano unzip -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo npm install pm2 -g
+#allow pm2 to start and stop after reboot
+pm2 startup
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+# make the needed directory
+mkdir ~/foundry
+mkdir ~/foundryuserdata
+# Install caddy for reverse proxy and https configuration
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
+# Enter Foundry timed url for download
+echo "please enter the foundry vtt timed download url for the NodeJS version"
+read tdurl
+wget --output-document ~/foundry/foundryvtt.zip "$tdurl"
+unzip ~/foundry/foundryvtt.zip -d ~/foundry/
+rm ~/foundry/foundryvtt.zip
+# Set up pm2 to start foundry vtt at system startup or reboot.
+pm2 start "node /home/ubuntu/foundry/resources/app/main.js --dataPath=/home/ubuntu/foundryuserdata" --name foundry
+pm2 save
+# Setting up Caddy reverse proxy
+curl -o Caddyfile https://raw.githubusercontent.com/aco-rt/Foundry-VTT-Oracle/main/Caddyfile
+sudo rm /etc/caddy/Caddyfile
+sudo mv Caddyfile /etc/caddy/Caddyfile
+echo "please enter the domain url your players will use to connect to your server"
+read vtturl
+sudo sed -i "s/your.hostname.com/$vtturl/g" /etc/caddy/Caddyfile
+sudo service caddy restart
+# Edit foundry options.json file to allow connections through proxy and 443
+sed -i 's/"proxyPort": null/"proxyPort": 443/g' /home/ubuntu/foundryuserdata/Config/options.json
+sed -i 's/"proxySSL": false/"proxySSL": true/g' /home/ubuntu/foundryuserdata/Config/options.json
+# Restarting the system to complete installation
+sleep 2
+clear
+echo "Restarting the system to complete installation"
+sleep 3
+sudo shutdown -r now
---
+++ foundryUpdatev13.sh
@@ -0,0 +1,46 @@
+#!/bin/bash    
+# chmod a+x /where/i/saved/it/foundryInstall.sh
+# Foundry Installation on Oracle
+# update the system and remove older packages
+sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean
+# To update to NodeJS 18, we should stop pm2 processes
+pm2 stop all
+# Remove the old start up path that was used in v12 and below, let's remove the process
+pm2 delete foundry
+# Remove the current pm2 from startup to allow for the upgrade.
+pm2 unstartup
+# Add the new method of installing NodeJS repository and update the installed version of NodeJS to v20.
+sudo apt install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install -y nodejs
+sudo apt update
+sudo apt upgrade -y
+# Set pm2 to use the upgraded version of NodeJS and set it to run on start again.
+npm rebuild -g pm2
+pm2 startup
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+# Restart any previously running pm2 managed processes.
+pm2 start "node /home/ubuntu/foundry/main.js --dataPath=/home/ubuntu/foundryuserdata" --name foundry
+pm2 save
+# This concludes the nodejs update.
+# Let's update foundry to v12 here
+# stopping foundry
+pm2 stop foundry
+# making a copy so we can go back if needed.
+mv foundry foundry-archive-v12
+# Getting and installing foundry
+mkdir ~/foundry
+# Enter Foundry timed url for download
+echo "please enter the foundry vtt v13 timed download url for the Linux NodeJS version"
+read tdurl
+wget --output-document ~/foundry/foundryvtt.zip "$tdurl"
+unzip ~/foundry/foundryvtt.zip -d ~/foundry/
+rm ~/foundry/foundryvtt.zip
+# Restart foundry using pm2
+pm2 start foundry
+echo "Restarting the system to complete installation"
+sleep 5
+sudo shutdown -r now
